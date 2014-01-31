2cfcd97f17cabf8ad26a5a374a17f40470d5cc56
anil <ulak09@gmail.com>
2014-01-31 19:17:22 +0530

    Updated the DAO an dthe Spring config files for fixing the persistence issue.

--- src/main/java/com/vin/web/impls/RegisterServiceDAOImpl.java
+++ src/main/java/com/vin/web/impls/RegisterServiceDAOImpl.java
@@ -3,26 +3,25 @@ package com.vin.web.impls;
 import org.hibernate.SessionFactory;
 import org.hibernate.classic.Session;
 
-import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
-public class RegisterServiceDAOImpl extends HibernateDaoSupport implements RegisterServiceDAO{
+public class RegisterServiceDAOImpl implements RegisterServiceDAO{
 
-//	private SessionFactory sessionFactory;
-//	
-//	public void setSessionFactory(SessionFactory sessionFactory) {
-//		this.sessionFactory = sessionFactory;
-//	}
+	private SessionFactory sessionFactory;
+	
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
 
 	@Override
 	public void createUser(UserEntity entity) {
 		
-//		Session session = sessionFactory.openSession();
-//		session.beginTransaction();
-//		session.save(entity);
-//		session.getTransaction().commit();
-//		sessionFactory.close();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(entity);
+		session.getTransaction().commit();
+		sessionFactory.close();
 		
-		getHibernateTemplate().save(entity);
+		
 		
 	}
 	
